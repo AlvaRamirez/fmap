2d9799f7ec54210b33852d71713e23904a7fc2ac
AlvaRamirez <55244792+AlvaRamirez@users.noreply.github.com>
2020-05-14 12:37:33 -0300

    ss

---
+++ assets/main.js
@@ -0,0 +1,142 @@
+let markersAll = [];
+
+window.initMap = () =>{
+  // The location of Uluru
+  const maimo = {lat: -34.610490, lng: -58.440860};
+  // The map, centered at Uluru
+  const map = new google.maps.Map(
+      document.getElementById('map'), 
+      {
+        zoom: 10,
+
+        center: maimo,
+        
+        styles: styles,
+        
+        streetViewControl: false,
+        
+        fullscreenControl: false,
+        
+        mapTypeControlOptions: {
+        
+        mapTypeIds: []
+        
+        },
+        
+        zoomControlOptions: {
+        
+        position: google.maps.ControlPosition.LEFT_CENTER
+        
+        }
+      });
+      fetchMarkers(map)
+
+
+    const handleFilterBarata = document.querySelector('.baratas');
+    const handleFilterChetas = document.querySelector('.chetas');
+    const handleFilterVeganas = document.querySelector('.veganas');
+    const handleFilterSinTacc = document.querySelector('.sinTacc');
+
+    //Eventos de click de los filtros
+    handleFilterBarata.addEventListener('click', (e) => {
+        e.preventDefault();
+        addMarkerFiltered('Baratas')
+    })
+    handleFilterChetas.addEventListener('click', (e) => {
+        e.preventDefault();
+        addMarkerFiltered('Chetas')
+    })
+    handleFilterVeganas.addEventListener('click', (e) => {
+        e.preventDefault();
+        addMarkerFiltered('Vegana')
+    })
+    handleFilterSinTacc.addEventListener('click', (e) => {
+        e.preventDefault();
+        addMarkerFiltered('Sin Tacc')
+    })
+
+    //Agrego los markers filtrados según filtro (markerType)
+    const addMarkerFiltered = (markerType) => {
+        console.log('clicked beer');
+      
+        markersAll.forEach((marker) => {
+            //console.log(marker)
+            marker.setMap(null); //Quita todos los markers del mapa
+        })
+        const markerFiltered = markersAll.filter((markerItem) => markerItem.customInfo === markerType)
+        markerFiltered.forEach((marker) => {
+            marker.setMap(map);
+        })
+    }
+}  
+
+
+const fetchMarkers = async (map) => {
+
+    try {
+    
+    const response = await fetch('assets/markers.json');
+    
+    const json = await response.json();
+    
+    json.forEach(marker => {
+        addMarker(map,marker);
+    console.log(marker)
+});
+    //var marker = new google.maps.Marker({position:{ lat:marker.lat,lng:marker.lng}, map: map });
+    
+    //marker.setMap(map);
+    
+    
+    
+    } catch (error) {
+    
+    console.log(error);
+     }
+    
+    }
+
+    const addMarker = (map, marker) => {
+        console.log(marker);
+        const {lat,lng,name, description,type, web, img} = marker;
+        const conetentString= `
+      
+        <div class="animated bounceIn">
+        <h2>${name}</h2>
+        <h3>${type}</h3>
+        <img class="imgHe" src="${img}"> 
+        <p>${description}</p> 
+        <a href="${web}"  target="_blank" class="web">Contacto</a>
+        </div>
+        `;
+
+        
+
+        const indoWindow= new google.maps.InfoWindow({
+            content: conetentString
+        });
+
+        const icons={
+            'Baratas': 'assets/img/baratas.png',
+            'Chetas': 'assets/img/chetas.png',
+            'Vegana': 'assets/img/veganas.png',
+            'Sin Tacc' : 'assets/img/sinTacc.png'
+        }
+        const markerItem = new google.maps.Marker(
+            {
+                position: { lat: lat, lng: lng },
+                icon: icons[type],
+                map: map,
+                customInfo: type
+            }
+        );
+        markerItem.setMap(map);
+        //Agrego evento de click en el marker, abre infowindow
+      markerItem.addListener ('click', function () {
+            indoWindow.open(map, markerItem);
+        });
+        //Agrego mi nuevo marker (objeto marker, no json marker, a mi array para filtros)
+        markersAll.push(markerItem);
+    }
+
+
---
+++ assets/mapStyles.js
@@ -0,0 +1,168 @@
+
+const styles= [
+    {
+        "featureType": "all",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "saturation": 36
+            },
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 40
+            }
+        ]
+    },
+    {
+        "featureType": "all",
+        "elementType": "labels.text.stroke",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 5
+            }
+        ]
+    },
+    {
+        "featureType": "all",
+        "elementType": "labels.icon",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 0
+            }
+        ]
+    },
+    {
+        "featureType": "administrative",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 0
+            },
+            {
+                "weight": 1.2
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 5
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 5
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 0
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 0
+            },
+            {
+                "weight": 0.2
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 5
+            }
+        ]
+    },
+    {
+        "featureType": "road.local",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 10
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 10
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 17
+            }
+        ]
+    }
+];
